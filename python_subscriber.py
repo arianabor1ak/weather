c161f8ca01a8c28057e7fdd60a5f0ae4d2b393e2
arianab <borlaka@carleton.edu>
2023-07-07 13:24:02 -0500

    cleaned up

--- python_subscriber.py
+++ python_subscriber.py
@@ -1,17 +1,14 @@
-import os
 import paho.mqtt.client as mqtt
-import psycopg2
-from dotenv import load_dotenv, find_dotenv
 import weather_db_wrapper
 
+# Create global instance of database access class
 weather_db = weather_db_wrapper.Weather_DB()
 
 # The callback for when the client receives a CONNACK response from the server.
+# Subscribing in on_connect() means that if we lose the connection and
+# reconnect then subscriptions will be renewed.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code: " + str(rc))
-    weather_db.db_connect()
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
+    print("Connected with result code: " + str(rc)) # turn this into a logging statement
     client.subscribe("CarletonWeatherTower")
 
 # The callback for when a PUBLISH message is received from the server.
@@ -31,12 +28,11 @@ def main():
     client.on_message = on_message
 
     # Connecting to broker
+	# Need to change once we have our own broker
     client.connect("test.mosquitto.org", 1883, 60)
 
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
-    # Other loop*() functions are available that give a threaded interface and a
-    # manual interface.
     client.loop_forever()
 
 if __name__ == "__main__":
