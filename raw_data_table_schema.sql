199d7d8572ee011a85c83e603902a1c5119c166e
arianab <borlaka@carleton.edu>
2023-07-07 13:25:46 -0500

    formatted_id added

--- raw_data_table_schema.sql
+++ raw_data_table_schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE raw_data_table (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     datetime_utc TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),                -- internal representation of time measurement was saved
+    formatted_id BIGINT,
     raw_master_string TEXT NOT NULL
 );
