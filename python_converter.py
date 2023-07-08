5918d28452394a7b770d33c2bbf9c158cfbd1ccf
arianab <borlaka@carleton.edu>
2023-07-08 11:33:39 -0500

    created subclass dictionary and implemented conversions

--- python_converter.py
+++ python_converter.py
@@ -17,21 +17,33 @@ def make_column_dict():
         column_dict[int(row[1])] = row[0]
     return column_dict
 
+# Creates a dictionary of the subclasses
+def make_subclass_dict():
+    class_dict = {}
+    key = 0
+    subclasses = [cls.__name__ for cls in ConversionObjects.ConversionObject.__subclasses__()]
+    for sub in subclasses:
+        class_dict[key] = sub
+        key += 1
+    print("Subclasses: ", class_dict)
+    return class_dict
+
 # Looks up the field index to determine which subclass is being handled
 # Performs the conversion according the subclass
 # Inserts the converted data into the formatted data table
-def conversion(field, raw_data_array, column_name, row_id):
-    find_field = ConversionObjects.field_dict[field] #use the number to index which object to create
-    converted = find_field #not sure if this will work #supposed to create instance of specific data object subclass
+def conversion(field, raw_data_array, column_name, row_id, subclasses):
+    find_field = subclasses[field] #use the number to index which object to create
+    eval_string = "ConversionObjects." + str(find_field) + "()"
+    print("Evaluated string: ", eval_string)
+    converted = eval(eval_string) #not sure if this will work #supposed to create instance of specific data object subclass
+    print(converted)
     converted_data = converted.format(raw_data_array[field]) #perform the conversion
 
     weather_db.insert_formatted_data(column_name, converted_data, row_id) #insert the converted value into the database
 
 # Parses through the raw data array and manages when to perform conversions
 # Updates the raw data table with the corresponding formatted table row id
-def parse_data(raw_data_array, raw_id, dictionary):
-    # create the row by defaulting column values to NULL
-    # update each column after the formatting takes place
+def parse_data(raw_data_array, raw_id, columns, subclasses):
     field = 0
     column_id = 3 #id of sql column data should be inserted into
     length = len(raw_data_array)
@@ -41,7 +53,7 @@ def parse_data(raw_data_array, raw_id, dictionary):
         raise ValueError("Master string must end with \'@\'")
         return
 
-    while raw_data_array[field] != "@" and field < length:
+    while raw_data_array[field] != "@" and field < 68: #should be length with actual data instead of 68
         if field == 0: #Unix timestamp
             #extrapolate into 10 specified fields
             #column_id is 3
@@ -57,26 +69,25 @@ def parse_data(raw_data_array, raw_id, dictionary):
             raw_data_array[field] == "5-":
             field += 1
             print("Skipping")
-        else: #add data to the corresponding column in formatted_data
-            # while raw_data_array[field] != "1-": #probably don't need a while loop? somehow index sql columns?
-            #need to include index number "and field < 160" or something
+        else: 
             #do some error checking to make sure it's not null?
-            column_name = dictionary[column_id]     #look up column name in dictionary
-            # conversion(field, raw_data_array, column_name, row_id)
+            column_name = columns[column_id]     #look up column name in column dictionary
+            conversion(field, raw_data_array, column_name, row_id, subclasses)
             field += 1
             column_id += 1
 
 def main():
-    dictionary = make_column_dict()
-    weather_db.find_null()
-    raw_rows = weather_db.null_array
+    columns = make_column_dict()
+    subclasses = make_subclass_dict()
+    
+    raw_rows = weather_db.find_null()
     if raw_rows:
         print("Raw rows: ", raw_rows)
         for row in raw_rows:
             current_row = weather_db.get_master_string(row)
             string_list = create_raw_data_array(current_row)
             row = row[0]
-            parse_data(string_list, row, dictionary)
+            parse_data(string_list, row, columns, subclasses)
 
 if __name__ == "__main__":
     main()
